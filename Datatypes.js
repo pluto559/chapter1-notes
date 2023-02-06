3ab56a7b371e0bd9f2bfb773c859988452d3288b
ic59 <ic59@fresnou.org>
2023-02-06 09:57:40 -0800

    initial commit

---
+++ Datatypes.js
@@ -0,0 +1,41 @@
+console.log(7);
+//This is a comment
+
+//Numbers
+//Integer
+console.log(-3);
+console.log(0);
+console.log(3);
+//Floating Point Number
+console.log(3.14);
+console.log(100.750);
+
+//Strings
+//Characters, Letters, symbols, spaces
+console.log("hello world");
+console.log('hello world');
+console.log(`hello world 123`);
+console.log('16.0');
+
+//Arithmetic Operators (Math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //shift-8 gives us *
+console.log(14/2);
+console.log(14%2); //% is called mod and means remainder after dividing
+console.log(15%10);
+
+//Concatenation: combine strings
+console.log("My name"+" is Daniel Zinzun.");
+console.log('My' + ' ' + 'age' + ' '+'is'+ ' '+ 15 + '.');
+console.log("Your age is\n15");
+
+//Template literal - plug in a calculated value
+//use backticks `` next to the 1
+console.log(`15 divided by 10 = ${15/10} axcvzxcvx`);
+console.log("Not"+" "+"today"+".");
+console.log("Your age /n is 14");
+console.log(`12+5=${12+5}`);
+console.log(`101 divided by 5 has the remainder of: ${101%5}`);
+console.log(`your age is ${14+3}`);
+console.log(`200 divided by 8 equals ${200/8}`);
---
+++ operators.js
@@ -0,0 +1,50 @@
+//Unary operators use only one value
+console.log(typeof 15);
+console.log(typeof "this string");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof'this');
+console.log(typeof"that");
+console.log(typeof"TRUE");
+
+//Binary operators use two values
+// Comparison operators
+console.log(10 > 5); //greater than
+console.log(10 < 5); //less than
+console.log(10 >= 5); //greater than or equal to
+console.log(10 <= 5); //less than or equal to
+console.log(10 == 5); //or they both equal? falsre
+console.log(10 == 10); //
+console.log(10 != 5); //are they different? false
+console.log(7==7);
+console.log(7!=7);
+console.log(7==10);
+console.log(7!=10);
+console.log(15>10);
+console.log(21>30);
+console.log(15>15);
+console.log(15>=15);
+console.log(15<=15);
+console.log(15<15);
+console.log(15<20);
+console.log(21<=50);
+console.log(23<=10);
+console.log(1>0&&10<=100);
+console.log(1>0&&10>=100);
+console.log(1<0&&10==100);
+console.log(1>0||10<=100);
+console.log(1>0||10>=100);
+console.log(1<0||10==100);
+console.log(10 > 5); //greater than
+console.log(10 < 5); //less than
+console.log(10 >= 
+console.log(10 <= 5);
+console.log(10 == 5);
+console.log(10 == 10);
+console.log(10 != 5);
+
+
+
+
+
+//Ternary Operators use three values
